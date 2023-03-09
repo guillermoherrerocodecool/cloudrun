b906de2efbba5ec998a3ca3adf95e3191e32de3b
Guillermo Herrero (EL) <guillermo.herrero@codecool.com>
2023-03-09 10:59:40 +0100

    Add empty backend structure

---
+++ backend/src/main/java/com/codecool/cloudrunbackend/CloudrunBackendApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.cloudrunbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudrunBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudrunBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/codecool/cloudrunbackend/CloudrunBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.cloudrunbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudrunBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
